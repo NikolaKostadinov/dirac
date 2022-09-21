e2a948183a1095e10470fd61695ab49a94bb0b5e
Nikola Kostadinov <nikolakostadinov@protonmail.com>
2022-09-21 23:11:52 +0300

    c++ are you ok ?

--- src/basis1.cpp
+++ src/basis1.cpp
@@ -17,11 +17,8 @@ Basis1<N>::Basis1(Vertex1<float>* input_vertices[])
 {
     _vertices = new Vertex1<float>[N];
 
-    /*for (int i = 0; i < N; i++)
-    {
-        float x = input_vertices[i]->x;
-        _vertices[i] = new Vertex1<float>(x);
-    }*/
+    for (int i = 0; i < N; i++)
+        _vertices[i] = new Vertex1<float>(input_vertices[i]->x);
 }
 
 template class Basis1<4>;
