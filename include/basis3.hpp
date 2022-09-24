e9adb82b7f59341d9bf8f2b09ee4f3de90cf1c53
Nikola Kostadinov <nikolakostadinov@protonmail.com>
2022-09-24 20:45:32 +0300

    flatlanders welcome to reality

--- include/basis3.hpp
+++ include/basis3.hpp
@@ -23,6 +23,26 @@ class Basis3
         unsigned int ySize();
         unsigned int zSize();
 
+        Vertex3<float> xOrigin();
+        Vertex3<float> yOrigin();
+        Vertex3<float> zOrigin();
+        Vertex3<float> xEnd();
+        Vertex3<float> yEnd();
+        Vertex3<float> zEnd();
+        Vertex3<float> xAxisAt(unsigned int _index_);
+        Vertex3<float> yAxisAt(unsigned int _index_);
+        Vertex3<float> zAxisAt(unsigned int _index_);
+
+        Vertex3<float>* toXOrigin();
+        Vertex3<float>* toYOrigin();
+        Vertex3<float>* toZOrigin();
+        Vertex3<float>* toXEnd();
+        Vertex3<float>* toYEnd();
+        Vertex3<float>* toZEnd();
+        Vertex3<float>* toXAxisAt(unsigned int _index_);
+        Vertex3<float>* toYAxisAt(unsigned int _index_);
+        Vertex3<float>* toZAxisAt(unsigned int _index_);
+
     private:
 
         unsigned int _nx;
--- src/basis3.cpp
+++ src/basis3.cpp
@@ -41,3 +41,117 @@ unsigned int Basis3::zSize()
 {
     return _nz;
 }
+
+Vertex3<float> Basis3::xOrigin()
+{
+    return *_toXOrigin;
+}
+
+Vertex3<float> Basis3::yOrigin()
+{
+    return *_toYOrigin;
+}
+
+Vertex3<float> Basis3::zOrigin()
+{
+    return *_toZOrigin;
+}
+
+Vertex3<float> Basis3::xEnd()
+{
+    if (_nx == 0U) return *_toXOrigin;
+    else return *(_toXOrigin + _nx - 1);
+}
+
+Vertex3<float> Basis3::yEnd()
+{
+    if (_ny == 0U) return *_toYOrigin;
+    else return *(_toYOrigin + _ny - 1);
+}
+
+Vertex3<float> Basis3::zEnd()
+{
+    if (_nz == 0U) return *_toZOrigin;
+    else return *(_toZOrigin + _nz - 1);
+}
+
+Vertex3<float> Basis3::xAxisAt(unsigned int _index_)
+{
+    if (_index_ <= _nx)
+        return *(_toXOrigin + _index_);
+    else
+        throw BASIS3_INDEX_TOO_LARGE;
+}
+
+Vertex3<float> Basis3::yAxisAt(unsigned int _index_)
+{
+    if (_index_ <= _ny)
+        return *(_toYOrigin + _index_);
+    else
+        throw BASIS3_INDEX_TOO_LARGE;
+}
+
+Vertex3<float> Basis3::zAxisAt(unsigned int _index_)
+{
+    if (_index_ <= _nz)
+        return *(_toZOrigin + _index_);
+    else
+        throw BASIS3_INDEX_TOO_LARGE;
+}
+
+Vertex3<float>* Basis3::toXOrigin()
+{
+    return _toXOrigin;
+}
+
+Vertex3<float>* Basis3::toYOrigin()
+{
+    return _toYOrigin;
+}
+
+Vertex3<float>* Basis3::toZOrigin()
+{
+    return _toZOrigin;
+}
+
+Vertex3<float>* Basis3::toXEnd()
+{
+    if (_nx == 0U) return _toXOrigin;
+    else return _toXOrigin + _nx - 1;
+}
+
+Vertex3<float>* Basis3::toYEnd()
+{
+    if (_ny == 0U) return _toYOrigin;
+    else return _toYOrigin + _ny - 1;
+}
+
+Vertex3<float>* Basis3::toZEnd()
+{
+    if (_nz == 0U) return _toZOrigin;
+    else return _toZOrigin + _nz - 1;
+}
+
+Vertex3<float>* Basis3::toXAxisAt(unsigned int _index_)
+{
+    if (_index_ <= _nx)
+        return _toXOrigin + _index_;
+    else
+        throw BASIS3_INDEX_TOO_LARGE;
+}
+
+Vertex3<float>* Basis3::toYAxisAt(unsigned int _index_)
+{
+    if (_index_ <= _ny)
+        return _toYOrigin + _index_;
+    else
+        throw BASIS3_INDEX_TOO_LARGE;
+}
+
+Vertex3<float>* Basis3::toZAxisAt(unsigned int _index_)
+{
+    if (_index_ <= _nz)
+        return _toZOrigin + _index_;
+    else
+        throw BASIS3_INDEX_TOO_LARGE;
+}
