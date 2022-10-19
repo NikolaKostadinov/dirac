cbd7318c18164edd4adfa3bd61f27b3be448c4ad
Nikola Kostadinov <nikolakostadinov@protonmail.com>
2022-10-19 22:05:30 +0300

    only the cat equation left

--- src/main.cpp
+++ src/main.cpp
@@ -8,28 +8,11 @@
 
 int main()
 {   
-    Base*      x     = new Base(-1, N, 1)  ;
-    /*Basis2*    basis = new Basis2(x, x)    ;
-    WaveFunc2* psi   = new WaveFunc2(basis);
+    Base*        x     = new Base(-1, N, 1)  ;
+    Basis3*      basis = new Basis3(x, x, x) ;
+    WaveFunc3*   psi   = new WaveFunc3(basis);
 
-    Complex probAmp[N][N];
-    for (uint32_t i = 0U; i < N; i++)
-        for (uint32_t j = 0U; j < N; j++)
-            probAmp[i][j] = Complex(cos(i), sin(j));
-
-    psi->setMass(1.0F);
-    psi->setNormValues(&probAmp[0][0]);
-
-    std::cout << psi->string() << std::endl << std::endl;
-
-    for (int i = 0; i < T; i++) psi->evolveFree(DT);
-
-    std::cout << psi->string() << std::endl;*/
-
-    Complex      probAmps             [N][N][N] ;
-    Basis3*      basis    = new Basis3(x, x, x) ;
-    WaveFunc3*   psi      = new WaveFunc3(basis);
-
+    Complex      probAmps[N][N][N];
     for (uint32_t i = 0u; i < N; i++)
         for (uint32_t j = 0u; j < N; j++)
             for (uint32_t k = 0u; k < N; k++)
