13b1c7656f720bdba84e3407a673831e42a3654c
Nikola Kostadinov <nikolakostadinov@protonmail.com>
2022-11-22 13:44:44 +0200

    hello vectors

--- include/vector2.hpp
+++ include/vector2.hpp
@@ -2,5 +2,40 @@
 
 class Vector2
 {
+    public:
 
+        Vector2();
+        virtual ~Vector2();
+
+        Vector2(float _x_, float _y_);
+
+        void     scale (float _factor_);
+        void     shrink(float _factor_);
+
+        Vector2  operator+ (const Vector2 _other_);
+        Vector2  operator- (const Vector2 _other_);
+        Vector2  operator* (const Vector2 _other_);
+        Vector2  operator/ (const Vector2 _other_);
+        Vector2& operator+=(const Vector2 _other_);
+        Vector2& operator-=(const Vector2 _other_);
+        Vector2& operator*=(const Vector2 _other_);
+        Vector2& operator/=(const Vector2 _other_);
+        bool     operator==(const Vector2 _other_);
+
+        float    x         (                     ) const;
+        float    y         (                     ) const;
+        float    length    (                     ) const;
+        float    dir       (                     ) const;
+        float    sq        (                     ) const;
+
+        float    inner     (const Vector2 _other_) const;
+        float    outer     (const Vector2 _other_) const;
+
+    protected:
+
+        float _x;
+        float _y;
 };
+
+float inner(const Vector2 _left_, const Vector2 _right_);
+float outer(const Vector2 _left_, const Vector2 _right_);
--- include/vector3.hpp
+++ include/vector3.hpp
@@ -2,5 +2,41 @@
 
 class Vector3
 {
+    public:
 
+        Vector3();
+        virtual ~Vector3();
+
+        Vector3(float _x_, float _y_, float _z_);
+
+        void     scale (float _factor_);
+        void     shrink(float _factor_);
+
+        Vector3  operator+ (const Vector3 _other_);
+        Vector3  operator- (const Vector3 _other_);
+        Vector3  operator* (const Vector3 _other_);
+        Vector3  operator/ (const Vector3 _other_);
+        Vector3& operator+=(const Vector3 _other_);
+        Vector3& operator-=(const Vector3 _other_);
+        Vector3& operator*=(const Vector3 _other_);
+        Vector3& operator/=(const Vector3 _other_);
+        bool     operator==(const Vector3 _other_);
+
+        float    x         (                     ) const;
+        float    y         (                     ) const;
+        float    z         (                     ) const;
+        float    length    (                     ) const;
+        float    sq        (                     ) const;
+
+        float    inner     (const Vector3 _other_) const;
+        Vector3  outer     (const Vector3 _other_) const;
+
+    protected:
+
+        float _x;
+        float _y;
+        float _z;
 };
+
+float inner(const Vector3 _left_, const Vector3 _right_);
+float outer(const Vector3 _left_, const Vector3 _right_);
